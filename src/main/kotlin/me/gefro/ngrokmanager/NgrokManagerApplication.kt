c1d74460eb245fdfb6559f543e02b39f8d6005ef
gefro47 <gefro47@gmail.com>
2023-11-28 17:05:44 +0100

    Initial commit

---
+++ src/main/kotlin/me/gefro/ngrokmanager/NgrokManagerApplication.kt
@@ -0,0 +1,11 @@
+package me.gefro.ngrokmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NgrokManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<NgrokManagerApplication>(*args)
+}
---
+++ src/test/kotlin/me/gefro/ngrokmanager/NgrokManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.gefro.ngrokmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NgrokManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
